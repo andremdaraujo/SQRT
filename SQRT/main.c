5a34847af10b53d1a8ebab155491584010c1df3d
andremdaraujo <andremdaraujo@gmail.com>
2022-02-05 01:09:08 -0300

    Added Quake III Arena fast inverse sqrt algorithm. Improved test program and used functions to improve code readability. Added error estimate to compare both methods. Exercise finished.

--- SQRT/main.c
+++ SQRT/main.c
@@ -1,57 +1,165 @@
+//
+//	André A. M. Araújo
+//	2022/02/04
+//
+//	This program is a solution to exercise 09 of the Classpert Course
+//	Making Embedded Systems by Elecia White:
+//
+//	Choose a small algorithm to implement in different ways. 
+//  Describe when/why you might use each. You can choose any small algorithm.
+// 
+//  The proposed program calculates the square root of an input using 2 methods.
+//  Comments throughout the code explain the details.
+// 
+//  Developed in Visual Studio Community 2022
+//
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
 
-#define PRECISION 0.001
+#define PRECISION		  0.0001	// Tolerance for solution against actual value
+#define MAX_ITERATIONS	100			// Maximum iterations for Newton's method
 
-double power(double base, uint32_t expoent)
+// Absolute value calculation for floating point variable
+float f_abs(float x)
 {
-	uint32_t i = 0;
-	double result = 1.0;
+	float y = 0.0;
 
-	while (i < expoent)
+	if (x < 0)
 	{
-		result = result * base;
-		i++;
-	}
-	return (result);
-}
-
-double f_abs(double num)
-{
-	double result = 0.0;
-
-	if (num < 0)
-	{
-		result = -num;
+		y = -x;
 	}
 	else
 	{
-		result = num;
+		y = x;
 	}
-	return result;
+	return y;
 }
 
+// Absolut error calculation for floating point variables
+float abs_error(float x, float y)
+{
+	float error = 0.0;
+
+	error = f_abs(x - y);
+
+	return error;
+}
+
+// Relative error calculation for floating point variables (x is the reference)
+float rel_error(float x, float y)
+{
+	float error = 0.0;
+
+	error = abs_error(x, y) / x;
+	
+	return error;
+}
+
+// Square root calculation based on Newton's successive approximation method.
+// This method has convergence assured and can be repeated until the desired
+// precision is met
+// Reference: https://en.wikipedia.org/wiki/Methods_of_computing_square_roots
+float newton_sqrt(float number)
+{
+	uint32_t i = 0;		// counter
+	float guess = 1;	// intermediate estimate of solution
+	float y = 0.0;		// solution
+
+	printf("Newton's successive approximation method: \n");
+
+	// Reiterate until desired precision is achieved or maximum iterations limit
+	for (i = 1; (rel_error(number, guess * guess) > PRECISION && i <= MAX_ITERATIONS); i++)
+	{
+		guess = (guess + number / guess) / 2;
+		printf("Iteration: %2d; guess: %f \n", i, guess);
+	}
+
+	y = guess;
+
+	printf("\n");
+
+	return y;
+}
+
+// Quake III Arena inverse square root, which is famous for being very fast
+// as it only uses one floating point division. The 2nd iteration is optional
+// to improve precision, but was commented out in the game code.
+// Reference: https://en.wikipedia.org/wiki/Fast_inverse_square_root
+// Actual code:
+float Q_rsqrt(float number)
+{
+	long i;
+	float x2, y;
+	const float threehalfs = 1.5F;
+
+	x2 = number * 0.5F;
+	y = number;
+	i = *(long*)&y;								// evil floating point bit level hacking
+	i = 0x5f3759df - (i >> 1);					// what the f***? 
+	y = *(float*)&i;
+	y = y * (threehalfs - (x2 * y * y));		// 1st iteration
+	y = y * ( threehalfs - ( x2 * y * y ) );	// 2nd iteration, this can be removed
+
+	return y;
+}
+
+// Short program to calculate the square root of a real (float) number using
+// Newton's and Quake III methods
+//
+// y = f(x) = sqrt(x)
+//
 void main(void)
 {
-	uint32_t n_iterations = 100;
-	uint32_t i = 0;	// counter
+	float x = 0.0;	// input
+	float y = 0.0;	// output
+	
+	printf("Square root estimate: \n y = f(x) = sqrt(x) \n\n");
+	printf("Enter floating point number (x): ");
 
-	double x = 2;	// function variable: f(x) = sqrt(x)
-	double s = x/2;	// solution/result
+	scanf_s("%f", &x);
 
-	double guess		= 0.001;
-	double result	= 0.0;
+	printf("\n");
 
-	// Square root calculation using Newton's successive approximation method
-	// (based on: https://pages.mtu.edu/~shene/COURSES/cs201/NOTES/chap06/sqrt-1.html)
-	for (i = 1; (i <= n_iterations && ( (f_abs(x - guess * guess)/x > PRECISION) ) ); i++)
-	{
-		guess = (guess + x / guess)/2;
-		printf("Iteration: %2d; g: %f \n", i, guess);
-	}
-	result = guess;
-	printf("sqrt(%f) = %f \n", x, result);
+	// Using Newton's method:
+	y = newton_sqrt(x);
+
+	printf("Newton'\s method: \n  y = sqrt(%f) = %f \n", x, y);
+	printf("Absolut error: %f; Relative error: %f %% \n",
+			abs_error(x, y * y),
+			rel_error(x, y * y)*100);
+	printf("    (x - y^2)         ");
+	printf("    (x - y^2) / x \n\n");
+	
+	// Using Quake III inverse square root:
+	y = 1 / Q_rsqrt(x);		// inverting again to get the sqrt
+
+	printf("Newton'\s method: \n  y = sqrt(%f) = %f \n", x, y);
+	printf("Absolut error: %f; Relative error: %f %% \n",
+			abs_error(x, y * y),
+			rel_error(x, y * y) * 100);
+	printf("    (x - y^2)         ");
+	printf("    (x - y^2) / x \n\n");
+
 	system("pause");
 }
+
+//
+// General comments about the algorithms:
+// 
+// Newton's method has greater precision because it reiterates until the desired
+// precision is met; but this might take many iterations, so precision has a 
+// direct impact on CPU use (and time to execute). However, to achieve even higher
+// precision, double precision floating point must be used.
+// 
+// Quake III Arena method is less precise, but it takes a predetermined number
+// of cycles to execute.
+// 
+// If an application needs a very responsive (quick) result and can tolerate
+// lower precision, the second method is a good option. 
+// 
+// However, if higher precision is needed and execution time is no so critical,
+// Newton's method can be used.
+//
